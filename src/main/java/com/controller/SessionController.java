4c9f06624ab8222c896b4013dacf46036b91f050
tejas <tejasshah@gmail.com>
2023-06-21 15:20:03 +0530

    insert user

--- src/main/java/com/controller/SessionController.java
+++ src/main/java/com/controller/SessionController.java
@@ -1,14 +1,19 @@
 package com.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.bean.UserBean;
+import com.dao.UserDao;
 
 @Controller
 public class SessionController {
 
+	@Autowired
+	UserDao userDao; 
+	
 	@GetMapping("/signup")
 	public String signup() {
 		return "Signup";// jsp Name no extension
@@ -16,6 +21,7 @@ public class SessionController {
 
 	@PostMapping("/saveuser")
 	public String saveUser(UserBean user) {
+		userDao.addUser(user);
 		return "Login";
 	}
 
